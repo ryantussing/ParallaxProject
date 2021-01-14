a27703f1b2b38a50b014e702a980e5c0e1572f47
ryantussing <56258028+ryantussing@users.noreply.github.com>
2021-01-14 09:47:27 -0500

    began working on animations (nothing functions)

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-var example;
+window.addEventListener("scroll", function() {
+    var distance = window.scrollY;
+    this.document.getElementById("mntbck").style.transform = `scaleX(${distance *1}); scaleY(${distance * 1});`;
+    this.console.log(distance);
+});
