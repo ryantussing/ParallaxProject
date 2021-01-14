3232978115839853ffcbdb87589356f92319b29f
ryantussing <56258028+ryantussing@users.noreply.github.com>
2021-01-14 13:39:50 -0500

    continued animations (css working)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", function() {
-    var distance = window.scrollY;
+    const distance = window.scrollY;
     this.document.getElementById("mntbck").style.transform = `scaleX(${distance *1}); scaleY(${distance * 1});`;
-    this.console.log(distance);
+    console.log(distance);
 });
